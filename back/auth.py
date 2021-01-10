770f7df36ecd7f39e599634449b45e23b79f2674
Kevinht2010 <kevin.hk.or2010@gmail.com>
2021-01-10 03:09:03 -0500

    Finished Authentication & Verification

--- back/auth.py
+++ back/auth.py
@@ -1,17 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 
 app = Flask(__name__)
 
+app.config.from_pyfile('config.cfg')
 
-@app.route('/api', methods=['GET', 'POST'])
+s = URLSafeTimedSerializer('abcd')
+
+mail = Mail(app)
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def auth():
+    if request.method == 'GET':
+        return {"action": "none"}
+    email = request.get_json()['email']
+    token = s.dumps(email, salt='email-confirm')
+    msg = Message('Email Confirmation', sender='graphtheorytool@gmail.com', recipients=[email])
+    link = "http://localhost:3000/" + "confirm_email/" + token
+    msg.body = 'Your email confirmation link is {}. Thanks and have a wonderful day!'.format(link)
+    mail.send(msg)
+    return {"token": token}
+
+
+@app.route('/confirm_email/<token>', methods=['GET', 'POST'])
+def confirm_email(token):
     if request.method == 'POST':
-        return {
-            'name': request.get_json()
-        }
-    return {
-        'name': "GET"
-    }
+        try:
+            email = s.loads(token, salt='email-confirm', max_age=600)
+        except SignatureExpired:
+            return {'status': "Expired"}
+        except:
+            return {'status': "Invalid"}
+        return {'status': "Accepted"}
+    return {'status': "None"}
 
 
 if __name__ == '__main__':
